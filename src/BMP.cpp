632c3e1c7b3dce28b7f6ffe5d6eeaabd468bee0d
Jan Kroeze <jcwkroeze@protonmail.com>
2020-06-28 15:24:29 +0200

    Add DIB header

--- src/BMP.cpp
+++ src/BMP.cpp
@@ -10,6 +10,14 @@ struct Header {
     uint16_t reserved[2];
     uint32_t offset;
 };
+
+struct DIB {
+    uint32_t size;
+    uint32_t width;
+    uint32_t height;
+    uint16_t planes;
+    uint16_t depth;
+};
 #pragma pack (pop)
 
 void loadBMP(uint8_t*& data, const char* path, uint32_t& x, uint32_t& y) {
@@ -17,5 +25,7 @@ void loadBMP(uint8_t*& data, const char* path, uint32_t& x, uint32_t& y) {
     fopen_s(&file, path, "r");
     Header header;
     readStruct(file, header);
+    DIB dib;
+    readStruct(file, dib);
     seek(file, header.offset);
 }
